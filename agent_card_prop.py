0fd5a43716b7c0438ad016c2e6c47e5daa46161d
Nizar Fathurohman <97506095+jarni66@users.noreply.github.com>
2023-10-28 21:44:47 +0700

    Add files via upload

---
+++ agent_card_prop.py
@@ -0,0 +1,187 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import json
+import requests
+import undetected_chromedriver as uc 
+import re
+import os
+
+def scrape_item():
+    data = {}
+    try:
+        title = soup_prop.find('h1',{'class':'title'}).text
+    except:
+        try:
+            title = soup_prop.find('h1').text.replace('\n','').strip()
+        except:
+            title = None
+
+    try:
+        address = soup_prop.find('p',{'class':'full-address__text'}).text
+    except:
+        try:
+            address = soup_prop.find('div',{'class':'property-detail'}).text.replace('\n','').strip()
+        except:
+            address = None
+
+    try:
+        desc = soup_prop.find('div',{'class':'description'}).text
+    except:
+        try:
+            desc = soup_prop.find('div',{'class':'listing-details-text compacted'}).text.replace('\n','').strip()
+        except:
+            desc = None
+
+    try:
+        price = soup_prop.find('h2',{'class':'amount'}).text
+    except:
+        try:
+            price = soup_prop.find('div',{'class':'property-value'}).text.replace('\n','').strip()
+        except:
+            price = None
+
+    try:
+        prop_type = soup_prop.find('div',{'class':'meta-table__item__value-text'}).text
+    except:
+        try:
+            prop_type = soup_prop.find('td',{'class':'value-block'}).text.replace('\n','').strip()
+        except:
+            prop_type = None
+
+    try:
+        agent_name = soup_prop.find('a',{'class':'actionable-link agent-name truncate-line'}).text
+    except:
+        agent_name = None
+
+    try:
+        agency = soup_prop.find('div',{'class':'agency'}).text
+    except:
+        try:
+            agency = soup_prop.find('div',{'class':'dev-name'}).text.replace('\n','').strip()
+        except:
+            agency = None
+
+
+    try:
+        agent_phone = soup_prop.find('a',{'class':'actionable-link btn-outline-secondary btn'}).get('href')
+    except:
+        agent_phone = None
+
+    try:
+        prop_cat = soup_prop.find('div',{'class':'label'}).text
+    except:
+        prop_cat = None
+
+    data['property_url'] = prop
+    data['title'] = title
+    data['address'] = address
+    data['desc'] = desc
+    data['price'] = price
+    data['prop_type'] = prop_type
+    data['prop_cat']= prop_cat
+    data['agent_name'] = agent_name
+    data['agency'] = agency
+
+
+    return data
+    
+
+chrome_options = uc.ChromeOptions() 
+chrome_options.headless = False  # Set headless to False to run in non-headless mode
+driver = uc.Chrome(options=chrome_options)
+
+link_done = []
+link_fail = []
+datas = []
+
+with open('agent/agent_info.json') as f:
+    out = json.load(f)
+agent_ids = [i['agent_id'] for i in out]
+
+agent_done = []
+
+def pageurl(x):
+    if x == 'resident_sell':
+        return f'https://www.rumah.com/properti-dijual/{page}?agent_id={agent_id}&market=residential'
+    elif x == 'resident_rent':
+        return f'https://www.rumah.com/properti-disewa/{page}?agent_id={agent_id}&market=residential'
+    elif x == 'commercial_sell':
+        return f'https://www.rumah.com/properti-dijual/{page}?agent_id={agent_id}&market=commercial'
+    else:
+        return f'https://www.rumah.com/properti-disewa/{page}?agent_id={agent_id}&market=commercial'
+    
+for agent_id in agent_ids:
+    agent_props = {'resident_sell':f'https://www.rumah.com/properti-dijual?agent_id={agent_id}&market=residential',
+                    'resident_rent':f'https://www.rumah.com/properti-disewa?agent_id={agent_id}&market=residential',
+                    'commercial_sell':f'https://www.rumah.com/properti-dijual?agent_id={agent_id}&market=commercial',
+                    'commercial_rent':f'https://www.rumah.com/properti-disewa?agent_id={agent_id}&market=commercial'}
+
+    for properties_link in agent_props.keys():
+        driver.get(url=agent_props[properties_link])
+        driver.set_page_load_timeout(20)
+        soup_page = BeautifulSoup(driver.page_source, 'html.parser')
+            
+        links_prop = soup_page.find_all('a',{'class':'nav-link'})
+
+        if links_prop != None:
+            try:
+                last_page = int(soup_page.find('ul',{'class':'pagination'}).find_all('li')[-2].text)
+                for page in range(1,last_page+1):
+                    url_ = pageurl(properties_link)
+                    driver.get(url=url_)
+                    soup_page_ = BeautifulSoup(driver.page_source, 'html.parser')
+                    links_prop = soup_page_.find_all('a',{'class':'nav-link'})
+                    links_prop = list(set([i.get('href') for i in links_prop]))
+                    scrape = 1
+                    for prop in links_prop:
+                        scrape += 1
+                        
+                        try:
+                            print(f"n_AGENT {agent_ids.index(agent_id)}, CAT {properties_link}, SCRAPE {scrape} {prop.split('/')[-1]}")
+                            driver.get(url=prop)
+                            soup_prop = BeautifulSoup(driver.page_source, 'html.parser')
+
+                            data = scrape_item()
+                            
+                            datas.append(data)
+
+                            with open('agent/agent_prop.json', 'w') as f:
+                                json.dump(datas, f)
+                        except:
+                            pass
+            except:
+                links_prop = soup_page.find_all('a',{'class':'nav-link'})
+                links_prop = [i.get('href') for i in links_prop]
+                scrape = 1
+                for prop in links_prop:
+                    scrape += 1
+                    try:
+                        print(f"n_AGENT {agent_ids.index(agent_id)}, CAT {properties_link}, SCRAPE {scrape}")
+                        
+                        driver.get(url=prop)
+                        soup_prop = BeautifulSoup(driver.page_source, 'html.parser')
+
+                        data = scrape_item()
+                        
+                        datas.append(data)
+
+                        with open('agent/agent_prop.json', 'w') as f:
+                            json.dump(datas, f)
+
+                    except:
+                        pass
+            
+        else :
+            pass
+    agent_done.append(agent_id)
+    datas_done = {'done':agent_done}
+    with open('agent/agent_prop_done.json', 'w') as f:
+        json.dump(datas_done, f)
+
